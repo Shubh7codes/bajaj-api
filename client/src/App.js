62c401cfc4978e6cef70dc349bd50558ee8b3034
Shubhransu Boral <Shubh7codes@gmail.com>
2024-09-22 16:45:33 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ function App() {
       setError('');
       console.log(jsonInput)
       const payload = JSON.parse(jsonInput); // Parse input
-      const res = await fetch('http://localhost:3001/bfhl', {
+      const res = await fetch('https://api-backend-bbrx.onrender.combfhl', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(payload),
