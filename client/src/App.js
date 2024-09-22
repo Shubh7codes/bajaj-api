6ee69133945ecef0e53d886700403d9b44dedb82
Shubh7Codes <shubh7codes@gmail.com>
2024-09-22 13:36:55 +0530

    jhekjf

---
+++ client/src/App.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+
+function App() {
+  const [jsonInput, setJsonInput] = useState('');
+  const [response, setResponse] = useState(null);
+  const [error, setError] = useState('');
+
+  const handleSubmit = async () => {
+    try {
+      setError('');
+      console.log(jsonInput)
+      const payload = JSON.parse(jsonInput); // Parse input
+      const res = await fetch('http://localhost:3001/bfhl', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(payload),
+      });
+      const data = await res.json();
+      setResponse(data);
+    } catch (err) {
+      setError('Invalid JSON or API error: ');
+      console.log(err);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Fullstack Project</h1>
+      <textarea
+        value={jsonInput}
+        onChange={(e) => setJsonInput(e.target.value)}
+        placeholder="Enter JSON here"
+      />
+      <br />
+      <button onClick={handleSubmit}>Submit</button>
+
+      {error && <p style={{ color: 'red' }}>{error}</p>}
+
+      {response && (
+        <div>
+          <h2>Response</h2>
+          <pre>{JSON.stringify(response, null, 2)}</pre>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
